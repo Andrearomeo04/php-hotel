42e226f52bc5ac94014ef2ffceb44573f27d4c74
andrearomeo04 <pisco004@gmail.com>
2024-09-06 01:59:26 +0200

    [UPD] cicled the array

--- index.php
+++ index.php
@@ -41,3 +41,28 @@
     ];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php foreach($hotels as $hotel){ ?>
+                    <p><?php echo $hotel['name']?></p>
+                    <p><?php echo $hotel['description']?></p>
+                    <p><?php echo $hotel['parking'] ? 'Parking available' : 'No parking available'?></p>
+                    <p><?php echo $hotel['vote']?></p>
+                    <p><?php echo $hotel['distance_to_center']?></p> 
+                <?php } ?>  
+            </div>
+        </div>
+    </div>
+</body>
+</html>
