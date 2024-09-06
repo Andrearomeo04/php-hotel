3be44130528b7638ca9941629f2b7c39b68ed6c9
andrearomeo04 <pisco004@gmail.com>
2024-09-06 02:38:07 +0200

    [UPD] added style to the table

--- index.php
+++ index.php
@@ -48,34 +48,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Description</th>
-                            <th>Parking</th>
-                            <th>Vote</th>
-                            <th>distance to center</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach($hotels as $hotel){ ?>
+                <h1 class="text-center">
+                    BOOTHELS
+                </h1>
+                <div class="d-flex justify-content-center">
+                    <table>
+                        <thead>
                             <tr>
-                                <td><?php echo $hotel['name']?></td>
-                                <td><?php echo $hotel['description']?></td>
-                                <td><?php echo $hotel['parking'] ? 'Available' : 'Not available'?></td>
-                                <td><?php echo $hotel['vote']?></td>
-                                <td><?php echo $hotel['distance_to_center']?></td> 
+                                <th>Name</th>
+                                <th>Description</th>
+                                <th>Parking</th>
+                                <th>Vote</th>
+                                <th>distance to center</th>
                             </tr>
-                        <?php } ?>  
-                    </tbody>
-                    </table>
+                        </thead>
+                        <tbody>
+                            <?php foreach($hotels as $hotel){ ?>
+                                <tr class="border-bottom border-secondary">
+                                    <td><?php echo $hotel['name']?></td>
+                                    <td><?php echo $hotel['description']?></td>
+                                    <td><?php echo $hotel['parking'] ? 'Available' : 'Not available'?></td>
+                                    <td><?php echo $hotel['vote']?></td>
+                                    <td><?php echo $hotel['distance_to_center']?></td> 
+                                </tr>
+                            <?php } ?>  
+                        </tbody>
+                        </table>
+                </div>
             </div>
         </div>
     </div>
